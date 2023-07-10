09ba0b19fda9a25558e98ba3b3c95616699fabd0
***** <gsood07@gmail.com>
2023-07-09 18:54:54 -0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-## Gringotts: Quick Paid API Infrastructure
+## Gringotts: Quick Monetization of Your API
 
-https://docs.google.com/document/d/1r3yEU6QiV5yDjyCmeCGXZh6d-_RfFvvKD4JO60Nd8KI/edit#
+Reduce time to monetize your API.
+
+### Solution
+
+We expand the API spec to add a cost field with price in terms of API credits and a translation for cost per credit. We also add an encrypted config to store your Stripe API Key.
+
+### Outcome
+
+Users can:
+
+1. Create an API Key
+2. Delete an API Key. Money is returned to the user account.
+3. Add money to an API Key
+4. Monitor their API credits remaining and the requests they made and how much each cost
+5. Get response from an API if API Key has credits remaining. Otherwise error 'too few credits.'
+
+Automatically create when user is created
+Add Credits
+
+
+
+Call API Endpoint: 
+Updates transactions table
+Updates user table (total remaining credits)
+	
+How?
+We log who made the call using the API Key
+Checks if there are remaining credits. If yes, then calls the API and updates the tables. Otherwise 
+
+
+
+
+### Authors
--- gringotts/main.py
+++ gringotts/main.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Define request payload model
+class PredictionRequest(BaseModel):
+    input_string: str
+    api_key: str
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Define response payload model
+class PredictionResponse(BaseModel):
+    output_string: str
 
+# API endpoint for prediction
+@app.post("/predict", response_model=PredictionResponse)
+async def predict(request: PredictionRequest):
+    # Perform authentication and authorization here
+    # Check if the API key is valid and has access rights
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+    # Mock prediction logic
+    output_string = f"Predicted: {request.input_string}"
+
+    # Return the prediction result
+    return PredictionResponse(output_string=output_string)
+
+# Handle invalid input payload
+@app.exception_handler(HTTPException)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(status_code=exc.status_code, content={"message": exc.detail})
+
+# Handle general exceptions
+@app.exception_handler(Exception)
+async def generic_exception_handler(request, exc):
+    return JSONResponse(status_code=500, content={"message": "Internal server error"})
